98820c42db5f4f61435288b82e8df4c40491c37f
lucigromo <luci.gromowski@gmail.com>
2025-06-17 22:22:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+pygame.init()
+pantalla = pygame.display.set_mode((1200, 900))
+pygame.display.set_caption("Sudoku")
+
+pantalla_actual = 1
+
+ejecutando = True
+while ejecutando:
+    if pantalla_actual == 1:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT: 
+                pygame.quit()
+                quit()
