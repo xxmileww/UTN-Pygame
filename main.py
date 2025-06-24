2c21dff8f11d33fa57d63f72f03b25269e41090f
lucigromo <luci.gromowski@gmail.com>
2025-06-24 16:33:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ pygame.display.set_icon(icono)
 info = pygame.display.Info()
 ancho_pantalla = info.current_w
 alto_pantalla = info.current_h
+ancho_ventana = int(ancho_pantalla * 0.8)
+alto_ventana = int(alto_pantalla * 0.8)
 
-pantalla = pygame.display.set_mode((int(ancho_pantalla * 0.8), int(alto_pantalla * 0.8)))
+fuente = pygame.font.SysFont("Calibri", 40)
+
+pantalla = pygame.display.set_mode((ancho_ventana, alto_ventana))
 
 pygame.display.set_caption("Sudoku")
 
@@ -24,6 +28,8 @@ pygame.mixer.music.load(musica_fondo)
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
 
+boton_jugar = pygame.Rect(ancho_ventana // 2 - 100, alto_ventana // 2 - 60, 200, 60)
+
 ejecutando = True
 while ejecutando:
     if pantalla_actual == 1:
@@ -31,8 +37,10 @@ while ejecutando:
             if evento.type == pygame.QUIT: 
                 pygame.quit()
                 quit()
-
+        pygame.draw.rect(pantalla, (87, 137, 173), boton_jugar, 2)
+        texto = fuente.render("Jugar", True, (23, 38, 48))
+        pantalla.blit(texto, (boton_jugar.x + 55, boton_jugar.y + 10))
+    pygame.display.flip()
     pantalla.fill(color_fondo)
     
     
-    pygame.display.flip()
