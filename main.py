c418bcddd8c2b5d7176fea37a872927d232072d8
lucigromo <luci.gromowski@gmail.com>
2025-07-01 01:04:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,9 +52,6 @@ ancho_botones = int(ancho_ventana * 0.15)
 alto_botones = int(alto_ventana * 0.1)
 ancho_total = 3 * ancho_botones + 2 * espacio_botones
 
-x_inicio = (ancho_ventana - ancho_total) // 2
-y_botones = int(alto_ventana * 0.5)
-
 ancho_volver = 200
 alto_volver = 60 
 
@@ -95,16 +92,16 @@ while ejecutando:
             if evento.type == pygame.QUIT: 
                 pygame.quit()
                 quit()
-        if evento.type == pygame.MOUSEBUTTONDOWN:
-            if rec_jugar.collidepoint(evento.pos):
-                pantalla_actual = 2
-            elif rec_ajustes.collidepoint(evento.pos):
-                pantalla_actual = 3
-            elif rec_puntaje.collidepoint(evento.pos):
-                pantalla_actual = 4
-            elif rec_salir.collidepoint(evento.pos):
-                pygame.quit()
-                quit()
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if rec_jugar.collidepoint(evento.pos):
+                    pantalla_actual = 2
+                elif rec_ajustes.collidepoint(evento.pos):
+                    pantalla_actual = 3
+                elif rec_puntaje.collidepoint(evento.pos):
+                    pantalla_actual = 4
+                elif rec_salir.collidepoint(evento.pos):
+                    pygame.quit()
+                    quit()
         boton_jugar = pygame.Rect(ancho_ventana * 0.35, alto_ventana * 0.25, ancho_ventana * 0.3, alto_ventana * 0.1)
         boton_ajustes = pygame.Rect(ancho_ventana * 0.35, alto_ventana * 0.40, ancho_ventana * 0.3, alto_ventana * 0.1)
         boton_puntaje = pygame.Rect(ancho_ventana * 0.35, alto_ventana * 0.55, ancho_ventana * 0.3, alto_ventana * 0.1)
@@ -121,11 +118,12 @@ while ejecutando:
         puntaje = fuente_botones.render("Puntaje", True, color_fuente)
         salir = fuente_botones.render("Salir", True, color_fuente)
 
-        pantalla.blit(titulo, (ancho_ventana * 0.35, alto_ventana * 0.07))
-        pantalla.blit(jugar, centrar_texto(boton_jugar, jugar))
-        pantalla.blit(ajustes, centrar_texto(boton_ajustes, ajustes))
-        pantalla.blit(puntaje, centrar_texto(boton_puntaje, puntaje))
-        pantalla.blit(salir, centrar_texto(boton_salir, salir))
+        rect_titulo = titulo.get_rect(center=(ancho_ventana // 2, alto_ventana * 0.07 + titulo.get_height() // 2))
+        pantalla.blit(titulo, rect_titulo)
+        pantalla.blit(jugar, jugar.get_rect(center=boton_jugar.center))
+        pantalla.blit(ajustes, ajustes.get_rect(center=boton_ajustes.center))
+        pantalla.blit(puntaje, puntaje.get_rect(center=boton_puntaje.center))
+        pantalla.blit(salir, salir.get_rect(center=boton_salir.center))
 
                 
     elif pantalla_actual == 2:
@@ -166,6 +164,7 @@ while ejecutando:
 
         x_inicio = (ancho_ventana - ancho_total) // 2
         y_botones = int(alto_ventana * 0.5)
+        
         boton_facil = pygame.Rect(x_inicio, y_botones, ancho_botones, alto_botones)
         boton_intermedio = pygame.Rect(x_inicio + ancho_botones + espacio_botones, y_botones, ancho_botones, alto_botones)
         boton_dificil = pygame.Rect(x_inicio + 2 * (ancho_botones + espacio_botones), y_botones, ancho_botones, alto_botones)
@@ -182,9 +181,9 @@ while ejecutando:
         intermedio = fuente_botones.render("Intermedio", True, color_fuente)
         dificil = fuente_botones.render("Difícil", True, color_fuente)
 
-        pantalla.blit(facil, centrar_texto(boton_facil, facil))
-        pantalla.blit(intermedio, centrar_texto(boton_intermedio, intermedio))
-        pantalla.blit(dificil, centrar_texto(boton_dificil, dificil))
+        pantalla.blit(facil, facil.get_rect(center=boton_facil.center))
+        pantalla.blit(intermedio, intermedio.get_rect(center=boton_intermedio.center))
+        pantalla.blit(dificil, dificil.get_rect(center=boton_dificil.center))
 
         aviso = fuente_aviso.render("Presione esc si desea volver atras", True, color_fuente)
         pantalla.blit(aviso, (ancho_ventana * 0.02, alto_ventana * 0.90))
@@ -225,12 +224,12 @@ while ejecutando:
 
         pygame.draw.rect(pantalla, color_boton_musica, boton_musica, border_radius=20)
         texto_musica = fuente_botones.render("Música", True, color_fuente)
-        pantalla.blit(texto_musica, centrar_texto(boton_musica, texto_musica))
+        pantalla.blit(texto_musica, texto_musica.get_rect(center=boton_musica.center))
 
         pygame.draw.rect(pantalla, color_botones, boton_cambiar_res, border_radius=20)
         res_actual = f"{ancho_ventana}x{alto_ventana}"
         texto_res = fuente_botones.render(f"Resolución: {res_actual}", True, color_fuente)
-        pantalla.blit(texto_res, centrar_texto(boton_cambiar_res, texto_res))
+        pantalla.blit(texto_res, texto_res.get_rect(center=boton_cambiar_res.center))
 
         aviso = fuente_aviso.render("Presione esc si desea volver atras", True, color_fuente)
         pantalla.blit(aviso, (ancho_ventana * 0.02, alto_ventana * 0.85))
@@ -247,7 +246,7 @@ while ejecutando:
         boton_volver = pygame.Rect(ancho_ventana * 0.05, alto_ventana * 0.85, ancho_ventana * 0.2, alto_ventana * 0.08)
         pygame.draw.rect(pantalla, color_botones, boton_volver, border_radius=20)
         texto_volver = fuente_botones.render("Volver", True, color_fuente)
-        pantalla.blit(texto_volver, centrar_texto(boton_volver, texto_volver))
+        pantalla.blit(texto_volver, texto_volver.get_rect(center=boton_volver.center))
 
     
     elif pantalla_actual == 5:
@@ -264,6 +263,12 @@ while ejecutando:
                     errores = 0                          
             if evento.type == evento_timer:
                 tiempo_juego += 1
+
+        tamaño_celda = int(alto_ventana * 0.08)
+        tamaño_tablero = tamaño_celda * 9
+
+        x_tablero = (ancho_ventana - tamaño_tablero) // 2 - int(ancho_ventana * 0.20) 
+        y_tablero = (alto_ventana - tamaño_tablero) // 2 - int(alto_ventana * 0.06)
 
         dibujar_sudoku( pantalla,tablero_juego, x_tablero, y_tablero, tamaño_celda, grosor_borde, color_negro, color_negro, color_celda_vacia, fuente_numero) 
 
@@ -279,208 +284,11 @@ while ejecutando:
 
         pygame.draw.rect(pantalla, color_botones, boton_volver, border_radius=20)
         texto_volver = fuente_botones.render("Volver", True, color_fuente)
-        pantalla.blit(texto_volver, centrar_texto(boton_volver, texto_volver))
+        pantalla.blit(texto_volver, texto_volver.get_rect(center=boton_volver.center))
 
         pygame.draw.rect(pantalla, color_botones, boton_reinicio, border_radius=20)
         texto_reinicio = fuente_botones.render("Reiniciar juego", True, color_fuente)
-        pantalla.blit(texto_reinicio, centrar_texto(boton_reinicio, texto_reinicio))
-
-
-    pygame.display.flip()
-    
-color_botones = (149, 215, 245)
-
-errores = 0
-errores_maximos = 3
-
-musica_fondo = "musica.mp3"
-pygame.mixer.music.load(musica_fondo)
-pygame.mixer.music.set_volume(0.2)
-pygame.mixer.music.play(-1)
-musica_activada = True
-
-alto_botones = 80
-ancho_botones = 250
-espacio_botones = 20
-
-ancho_volver = 200
-alto_volver = 60 
-
-x_volver = ancho_ventana - ancho_volver - 300  
-y_volver = alto_ventana - alto_volver - 50    
-
-ancho_reinicio = 245
-alto_reinicio = 60
-
-x_reinicio = ancho_ventana - ancho_reinicio - 600
-y_reinicio = alto_ventana - alto_reinicio - 50
-
-boton_jugar = pygame.Rect(ancho_pantalla * 0.33, alto_pantalla * 0.28, ancho_botones, alto_botones) 
-boton_ajustes = pygame.Rect(boton_jugar.x, boton_jugar.y + alto_botones + espacio_botones, ancho_botones, alto_botones)
-boton_puntaje = pygame.Rect(boton_ajustes.x, boton_ajustes.y + alto_botones + espacio_botones, ancho_botones, alto_botones)
-boton_salir = pygame.Rect(boton_ajustes.x, boton_puntaje.y + alto_botones + espacio_botones, ancho_botones, alto_botones)
-boton_volver = pygame.Rect(x_volver, y_volver, ancho_volver, alto_volver)
-boton_cambiar_res = pygame.Rect(ancho_pantalla * 0.33, alto_pantalla * 0.28, ancho_botones, alto_botones)
-boton_musica = pygame.Rect(boton_cambiar_res.x, boton_cambiar_res.y + alto_botones + espacio_botones, ancho_botones, alto_botones)
-boton_reinicio = pygame.Rect(x_reinicio, y_reinicio, ancho_reinicio, alto_reinicio)
-
-
-boton_facil = pygame.Rect(ancho_pantalla * 0.10, alto_pantalla * 0.35, ancho_botones, alto_botones)
-boton_intermedio = pygame.Rect(ancho_pantalla * 0.32, boton_facil.y, ancho_botones, alto_botones)
-boton_dificil = pygame.Rect(ancho_pantalla * 0.55, boton_facil.y, ancho_botones, alto_botones)
-
-evento_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(evento_timer, 1000)  
-
-color_blanco =(255,255,255)
-color_negro = (0, 0, 0)
-color_gris = (100, 100, 100)
-color_azul = (70, 70 ,120)
-color_celda_vacia= (245, 245, 245)
-
-tamaño_celda = 40
-grosor_borde = 1
-tamaño_tablero = tamaño_celda * 9
-x_tablero = (ancho_ventana - tamaño_tablero) //4
-y_tablero = (alto_ventana - tamaño_tablero) //2
-
-
-base_tablero = []
-tablero_juego = []
-dificultad_actual ="facil"
-tiempo_juego = 0
-
-
-ejecutando = True
-while ejecutando:
-    pantalla.blit(fondo_imagen, (0, 0))
-    if pantalla_actual == 1:
-        rec_jugar, rec_ajustes, rec_puntaje, rec_salir = pantalla_inicio(pantalla,color_botones,color_fuente,fuente_titulo,fuente_botones,ancho_pantalla,alto_pantalla,boton_jugar,boton_ajustes,boton_puntaje,boton_salir)
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT: 
-                pygame.quit()
-                quit()
-        if evento.type == pygame.MOUSEBUTTONDOWN:
-            if rec_jugar.collidepoint(evento.pos):
-                pantalla_actual = 2
-            elif rec_ajustes.collidepoint(evento.pos):
-                pantalla_actual = 3
-            elif rec_puntaje.collidepoint(evento.pos):
-                pantalla_actual = 4
-            elif rec_salir.collidepoint(evento.pos):
-                pygame.quit()
-                quit()
-                
-    elif pantalla_actual == 2:
-        rec_facil, rec_intermedio, rec_dificil = pantalla_dificultad(pantalla, color_botones, color_fuente, fuente_botones, ancho_pantalla, alto_pantalla, boton_facil, boton_intermedio, boton_dificil, fuente_textos, fuente_aviso)
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT: 
-                pygame.quit()
-                quit()
-            if evento.type == pygame.MOUSEBUTTONDOWN:
-                lista_numeros = [1,2,3,4,5,6,7,8,9]
-                base_tablero = generar_tablero_sudoku (lista_numeros)
-            
-                if rec_facil.collidepoint(evento.pos):
-                    dificultad_actual = "Fácil"
-                    tablero_juego = ocultar_numeros(base_tablero, dificultad_actual) 
-                    tiempo_juego = 0
-                    errores = 0
-                    pantalla_actual = 5
-                elif rec_intermedio.collidepoint(evento.pos):
-                    dificultad_actual = "Intermedio"
-                    tablero_juego = ocultar_numeros(base_tablero, dificultad_actual) 
-                    tiempo_juego = 0
-                    errores = 0
-                    pantalla_actual = 5
-                elif rec_dificil.collidepoint(evento.pos):
-                    dificultad_actual = "Difícil"
-                    tablero_juego = ocultar_numeros(base_tablero, dificultad_actual) 
-                    tiempo_juego = 0
-                    errores = 0
-                    pantalla_actual = 5
-
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_ESCAPE:
-                    pantalla_actual = 1
-                    
-    elif pantalla_actual == 3:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT: 
-                pygame.quit()
-                quit()
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_ESCAPE:
-                    pantalla_actual = 1
-
-            if evento.type == pygame.MOUSEBUTTONDOWN:
-                if boton_musica.collidepoint(evento.pos):
-                    if musica_activada:
-                        pygame.mixer.music.pause()
-                        musica_activada = False
-                    else:
-                        pygame.mixer.music.unpause()
-                        musica_activada = True
-        if musica_activada:
-            color_boton_musica = color_botones
-        else:
-            color_boton_musica = (255, 100, 100)
-       
-        pygame.draw.rect(pantalla, color_boton_musica, boton_musica, border_radius=20)
-        texto_musica = fuente_botones.render("Música", True, color_fuente)
-        pantalla.blit(texto_musica, (boton_musica.x + 80, boton_musica.y + 20))
-       
-        pygame.draw.rect(pantalla, color_botones, boton_cambiar_res, border_radius=20)
-        texto_res = fuente_botones.render("Resolución", True, color_fuente)
-        pantalla.blit(texto_res, (boton_cambiar_res.x + 50, boton_cambiar_res.y + 20))
-        
-        aviso = fuente_aviso.render("Presione esc si desea volver atras", True, color_fuente)
-        pantalla.blit(aviso, (ancho_pantalla * 0.02, alto_pantalla * 0.70))
-
-    elif pantalla_actual == 4:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT: 
-                pygame.quit()
-                quit()
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_ESCAPE:
-                    pantalla_actual = 1
-
-        aviso = fuente_aviso.render("Presione esc si desea volver atras", True, color_fuente)
-        pantalla.blit(aviso, (ancho_pantalla * 0.02, alto_pantalla * 0.70))
-    
-    elif pantalla_actual == 5:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if evento.type == pygame.MOUSEBUTTONDOWN:
-                if boton_volver.collidepoint(evento.pos):
-                    pantalla_actual = 1
-                elif boton_reinicio.collidepoint(evento.pos):
-                    tablero_juego = ocultar_numeros(base_tablero, dificultad_actual)
-                    tiempo_juego = 0
-                    errores = 0                          
-            if evento.type == evento_timer:
-                tiempo_juego += 1
-
-        dibujar_sudoku( pantalla,tablero_juego, x_tablero, y_tablero, tamaño_celda, grosor_borde, color_negro, color_negro, color_celda_vacia, fuente_numero) 
-
-        
-        minutos = tiempo_juego // 60
-        segundos = tiempo_juego % 60
-        texto_timer = fuente_timer.render(f"Tiempo: {minutos:02}:{segundos:02}", True, color_fuente)
-        pantalla.blit(texto_timer, (ancho_ventana - 550, 40))
-        texto_errores = fuente_timer.render(f"Errores: {errores}/{errores_maximos}", True, color_fuente)
-        pantalla.blit(texto_errores, (ancho_ventana - 545, 100))
-        
-        pygame.draw.rect(pantalla, color_botones, boton_volver, border_radius=20)
-        texto_volver = fuente_botones.render("Volver", True, color_fuente)
-        pantalla.blit(texto_volver, (boton_volver.x + 55, boton_volver.y + 20))
-
-        pygame.draw.rect(pantalla, color_botones, boton_reinicio, border_radius= 20)            
-        texto_reinicio = fuente_botones.render("reiniciar juego", True, color_fuente)
-        pantalla.blit(texto_reinicio, (boton_reinicio.x + 20, boton_reinicio.y +20))
+        pantalla.blit(texto_reinicio, texto_reinicio.get_rect(center=boton_reinicio.center))
 
 
     pygame.display.flip()
